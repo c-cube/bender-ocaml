d1b75b5360d6624e41ebad4df2c94cfc9d0fe3f3
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-02-21 00:39:32 +0100

    bugfixes

--- src/clients/database.ml
+++ src/clients/database.ml
@@ -31,7 +31,9 @@ let stmt_to_list stmt =
 (* execute statement parametrized by the array of arguments *)
 let exec_a db stmt a =
   let stmt = Sqlite3.prepare db stmt in
-  Array.iteri (fun i x -> ignore (Sqlite3.bind stmt (i+1) x)) a
+  Array.iteri (fun i x -> ignore (Sqlite3.bind stmt (i+1) x)) a;
+  ignore (Sqlite3.step stmt);
+  ()
 
 (* execute statement with 1 param, return rows *)
 let exec1 db stmt x : _ list =
@@ -46,8 +48,11 @@ let pick_list = function
       let i = Random.int (List.length l) in
       Some (List.nth l i)
 
+let norm_key k = String.trim k |> String.lowercase
+
 (* search for replies to [msg] and send them to [reply_to] *)
 let search_for_replies t ~reply_to msg =
+  let msg = norm_key msg in
   (* find corresponding replies *)
   let l = exec1 t.db "select reply from phrases where quote=?" (D.TEXT msg) in
   match pick_list l with
@@ -77,11 +82,13 @@ let update t ~reply_to msg =
           end
       | "add" ->
           let quote, reply = Scanf.sscanf arg "%s@|%s" mk_pair in
-          Logs.info (fun k->k "add fact %s -> %s" quote reply);
-          exec_a t.db "insert into phrases values (?, ?)"
-            [| D.TEXT quote; D.TEXT arg |];
+          let quote = norm_key quote and reply = String.trim reply in
+          Logs.info (fun k->k "add fact `%s` -> `%s`" quote reply);
+          exec_a t.db "insert into phrases values (?, ?); "
+            [| D.TEXT quote; D.TEXT reply |];
           replyf "added `%s` -> `%s`" quote reply
       | "remove" ->
+          let arg = norm_key arg in
           Logs.info (fun k->k "remove facts for `%s`" arg);
           exec_a t.db "delete from phrases where quote=?" [| D.TEXT arg |];
           let n = Sqlite3.changes t.db in
@@ -122,8 +129,7 @@ let reporter ppf =
   let report _src level ~over k msgf =
     let k _ = over(); k() in
     let with_stamp h k ppf fmt =
-      Format.kfprintf k ppf ("@[%a" ^^ fmt ^^ "@]@.")
-        Logs.pp_header (level, h)
+      Format.kfprintf k ppf ("@[%a " ^^ fmt ^^ "@]@.") Logs.pp_header (level, h)
     in
     msgf @@ fun ?header ?tags:_ fmt -> with_stamp header k ppf fmt
   in
@@ -132,14 +138,15 @@ let reporter ppf =
 let () =
   Arg.parse opts (fun _ -> ()) "database [options]";
   Logs.set_reporter (reporter Format.std_formatter);
-  Format.printf "open DB %s@." !db_;
+  Logs.set_level (Some Logs.Debug);
+  Logs.info (fun k->k "open DB `%s`" !db_);
   let db = Sqlite3.db_open !db_ in
   init_db db;
-  Format.printf "connecting to bender...@.";
+  Logs.info (fun k->k "connecting to bender...");
   let c = C.connect_exn () in
-  Format.printf "connected@.";
+  Logs.info (fun k->k "connected");
   let t = { db; client=c; } in
   C.loop_exn c
     ~f:(fun e ->
-      Logs.info (fun k->k "@[<2>receive event@ @[%a@]@]@." C.pp_event e);
+      Logs.debug (fun k->k "@[<2>receive event@ @[%a@]@]@." C.pp_event e);
       handle_event t e)
